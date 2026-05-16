a7612d6bef92613182d15597cb26d237fe49d8d3
suzan-sheikh <mdsuzanskh@gmail.com>
2026-05-16 18:24:42 +0600

    JSON data added

--- src/type.ts
+++ src/type.ts
@@ -2,3 +2,11 @@ import type { IncomingMessage, ServerResponse } from "node:http";
 
 export type Res = ServerResponse;
 export type Req = IncomingMessage;
+
+export interface Order {
+  id: string;
+  customer: string;
+  quantity: number;
+  food: string;
+  price: number;
+}
